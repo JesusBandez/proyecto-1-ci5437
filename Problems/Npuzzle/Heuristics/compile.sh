2ab38d698667040eef7dee5b2df17feb85344fcf
rcgamboan <rcgamboan@gmail.com>
2023-06-10 13:38:59 -0400

    cambio pdbs Npuzzles

--- Problems/Npuzzle/Heuristics/compile.sh
+++ Problems/Npuzzle/Heuristics/compile.sh
@@ -1,3 +1,8 @@
-make 15puzzleAbstraction.out
-make 24puzzleAbstraction.out
+make 15puzzleAbstraction1.out
+make 15puzzleAbstraction2.out
+make 15puzzleAbstraction3.out
+make 24puzzleAbstraction1.out
+make 24puzzleAbstraction2.out
+make 24puzzleAbstraction3.out
+make 24puzzleAbstraction4.out
 make clean
