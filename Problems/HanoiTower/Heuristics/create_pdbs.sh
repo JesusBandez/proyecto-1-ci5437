2166a42522a001ec6c22a1d1811749fbc07ee7ef
Jesus_B <17-10046@usb.ve>
2023-06-10 12:46:09 -0400

    18 disk pdbs

--- Problems/HanoiTower/Heuristics/create_pdbs.sh
+++ Problems/HanoiTower/Heuristics/create_pdbs.sh
@@ -1,3 +1,8 @@
 ./HanoiTower12DiskAbstraction1BottomDiskLess.out < ../Definitions/GoalState12Abstraction1BottomDiskLess.txt 12DiskAbstraction1BottomDiskLess.pdb
 ./HanoiTower12DiskAbstraction11TopDiskLess.out < ../Definitions/GoalState12Abstraction11TopDiskLess.txt 12DiskAbstraction11TopDiskLess.pdb
 
+./HanoiTower14DiskAbstraction3BottomDiskless.out < ../Definitions/GoalState14Abstraction3BottomDiskLess.txt 14DiskAbstraction3BottomDiskLess.pdb
+./HanoiTower14DiskAbstraction11TopDiskless.out < ../Definitions/GoalState14Abstraction11TopDiskLess.txt 14DiskAbstraction11TopDiskLess.pdb
+
+./HanoiTower18DiskAbstraction7BottomDiskless.out < ../Definitions/GoalState18Abstraction7BottomDiskLess.txt 18DiskAbstraction7BottomDiskLess.pdb
+./HanoiTower18DiskAbstraction11TopDiskLess.out < ../Definitions/GoalState18Abstraction11TopDiskLess.txt 18DiskAbstraction11TopDiskLess.pdb
