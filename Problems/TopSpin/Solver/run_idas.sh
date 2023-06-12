5a9acc05dbb0605e2811afa19d0951d03ef870f8
Jesus_B <17-10046@usb.ve>
2023-06-12 13:06:09 -0400

    Max of multiples pdbs ida

--- Problems/TopSpin/Solver/run_idas.sh
+++ Problems/TopSpin/Solver/run_idas.sh
@@ -1,8 +1,8 @@
 #el .pdb debe estar en Definitions, en este caso 15puzzleAbstraction1.pdb
-./TopSpin12Token.ida ../Definitions/TopSpin12EvenToken < ../Definitions/GoalState12.txt
+./TopSpin12Token.ida ../Definitions/TopSpin12EvenToken ../Definitions/TopSpin12OddToken < ../Definitions/GoalState12.txt
 
-./TopSpin14Token.ida ../Definitions/TopSpin14EvenToken < ../Definitions/GoalState14.txt
+./TopSpin14Token.ida ../Definitions/TopSpin14EvenToken ../Definitions/TopSpin14OddToken < ../Definitions/GoalState14.txt
 
-./TopSpin17Token.ida ../Definitions/TopSpin17EvenToken < ../Definitions/GoalState17.txt
+./TopSpin17Token.ida ../Definitions/TopSpin17EvenToken ../Definitions/TopSpin17OddToken < ../Definitions/GoalState17.txt
 
 
