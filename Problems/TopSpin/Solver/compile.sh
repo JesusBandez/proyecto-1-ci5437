cc6d7f7140a2d8082dc904912857c4089385e68d
Jesus_B <17-10046@usb.ve>
2023-06-12 13:44:38 -0400

    a star make

--- Problems/TopSpin/Solver/compile.sh
+++ Problems/TopSpin/Solver/compile.sh
@@ -9,3 +9,11 @@ mv ./TopSpin14Token.ida ../Solver
 make TopSpin17Token.ida
 mv ./TopSpin17Token.ida ../Solver
 
+make TopSpin12Token.a
+mv ./TopSpin12Token.a ../Solver
+
+make TopSpin14Token.a
+mv ./TopSpin14Token.a ../Solver
+
+make TopSpin17Token.a
+mv ./TopSpin17Token.a ../Solver
