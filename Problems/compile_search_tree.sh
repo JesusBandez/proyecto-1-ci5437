8610516dc5855d7ec35bae2f5529f6ebdc1c0749
Jesus_B <17-10046@usb.ve>
2023-06-12 00:59:37 -0400

    all compile and run

---
+++ Problems/compile_search_tree.sh
@@ -0,0 +1,13 @@
+pwd
+cd ./HanoiTower/SearchTree
+./compile.sh
+make clean
+cd ../../Npuzzle/SearchTree
+./compile.sh
+make clean
+cd ../../Rubik/SearchTree
+./compile.sh
+make clean
+cd ../../TopSpin/SearchTree
+./compile.sh
+make clean
---
+++ Problems/run_search_tree_benchmark.sh
@@ -0,0 +1,14 @@
+pwd
+cd ./HanoiTower/SearchTree
+./run_benchmark.sh
+make clean
+cd ../../Npuzzle/SearchTree
+./run_benchmark.sh
+make clean
+cd ../../Rubik/SearchTree
+./run_benchmark.sh
+make clean
+cd ../../TopSpin/SearchTree
+./run_benchmark.sh
+make clean
+
