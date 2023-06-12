487213d6d73883bb30749b1b131ba58893ba2c0c
Jesus_B <17-10046@usb.ve>
2023-06-12 17:20:27 -0400

    N puzzle ida

---
+++ Problems/Npuzzle/Solver/create_pdb.sh
@@ -0,0 +1,13 @@
+cd ../Heuristics
+#./create_pdbs.sh
+
+mv ./24puzzleAbstraction1.pdb ../Definitions/
+mv ./24puzzleAbstraction2.pdb ../Definitions/
+mv ./24puzzleAbstraction3.pdb ../Definitions/
+mv ./24puzzleAbstraction4.pdb ../Definitions/
+mv ./24puzzleAbstraction5.pdb ../Definitions/
+
+mv ./15puzzleAbstraction1.pdb ../Definitions/
+mv ./15puzzleAbstraction2.pdb ../Definitions/
+mv ./15puzzleAbstraction3.pdb ../Definitions/
+
